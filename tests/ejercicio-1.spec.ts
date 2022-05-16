a8f6c96373353801622bf2ad14e1ece166765c9f
Marco Cabrera <marco.cabrerahdez@gmail.com>
2022-05-16 13:07:14 +0100

    Update de ficheros

--- tests/ejercicio-1.spec.ts
+++ tests/ejercicio-1.spec.ts
@@ -7,7 +7,7 @@ import {WithoutPipeMethod} from '../src/ejercicio-2/metodo-2';
 describe('Pruebas clase MehtodTemplate', () => {
   describe('Pruebas clase WithPipeMethod', () => {
     it('catGrepCommand() Match found', (done) => {
-      const pipe: WithPipeMethod = new WithPipeMethod('prueba.txt', 'Hola');
+      const pipe: WithPipeMethod = new WithPipeMethod('tests/prueba.txt', 'Hola');
       pipe.on('close', (message) => {
         expect(message.toString()).to.be.equal(chalk.green('Total matches: 1'));
         done();
@@ -16,7 +16,7 @@ describe('Pruebas clase MehtodTemplate', () => {
     });
 
     it('pipesFind() Match not found', (done) => {
-      const pipe: WithPipeMethod = new WithPipeMethod('prueba.txt', 'Pepe');
+      const pipe: WithPipeMethod = new WithPipeMethod('tests/prueba.txt', 'Pepe');
       pipe.on('close', (message) => {
         // eslint-disable-next-line max-len
         expect(message.toString()).to.be.equal(chalk.red('No matches found for Pepe'));
@@ -29,7 +29,7 @@ describe('Pruebas clase MehtodTemplate', () => {
   describe('Pruebas clase WithoutPipeMethod', () => {
     it('catGrepCommand() Match found', (done) => {
       // eslint-disable-next-line max-len
-      const nopipe: WithoutPipeMethod = new WithoutPipeMethod('prueba.txt', 'Hola');
+      const nopipe: WithoutPipeMethod = new WithoutPipeMethod('tests/prueba.txt', 'Hola');
       nopipe.on('close', (message) => {
         expect(message.toString()).to.be.equal(chalk.green('Total matches: 1'));
         done();
